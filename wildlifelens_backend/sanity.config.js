1663756b41e3ea88948143d4be8573649c9df8a1
jeramief <109633173+jeramief@users.noreply.github.com>
2023-09-03 00:49:35 -0400

    adding schemas

--- wildlifelens_backend/sanity.config.js
+++ wildlifelens_backend/sanity.config.js
@@ -1,14 +1,14 @@
 import {defineConfig} from 'sanity'
 import {deskTool} from 'sanity/desk'
 import {visionTool} from '@sanity/vision'
-import {schemaTypes} from './schemas'
+import {schemaTypes} from './schemas/schema'
 
 export default defineConfig({
   name: 'default',
   title: 'Wild Life Lens',
 
   projectId: 'hj9gs7g8',
-  dataset: 'production',
+  dataset: 'default',
 
   plugins: [deskTool(), visionTool()],
 
