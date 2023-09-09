7f61d57f83aa84c6a8a21ccf252c6709400e60e0
jeramief <109633173+jeramief@users.noreply.github.com>
2023-09-09 14:08:21 -0400

    added categories

--- wildlifelens_frontend/src/components/Sidebar.jsx
+++ wildlifelens_frontend/src/components/Sidebar.jsx
@@ -10,6 +10,15 @@ const isNotActiveStyle =
 const isActiveStyle =
   "flex items-center px-5 gap-3 font-extrabold border-r-2 border-black transition-all duration-200 ease-in-out capitalize";
 
+const categories = [
+  { name: "Wild Animals" },
+  { name: "Plants" },
+  { name: "Sky" },
+  { name: "Mountains" },
+  { name: "Pets" },
+  { name: "Other" },
+];
+
 const Sidebar = ({ user, closeToggle }) => {
   const handleCloseSidebar = () => {
     if (closeToggle) closeToggle(false);
@@ -26,15 +35,52 @@ const Sidebar = ({ user, closeToggle }) => {
             onClick={handleCloseSidebar}
           />
         </Link>
+
         <div className="flex flex-col gap-5">
           <NavLink
             to="/"
             className={({ isActive }) =>
               isActive ? isActiveStyle : isNotActiveStyle
             }
-          ></NavLink>
+            onClick={handleCloseSidebar}
+          >
+            <RiHomeFill />
+            Home
+          </NavLink>
+
+          <h3 className="mt-2 px-5 text-base 2xl:text-xl">
+            Discover categories
+          </h3>
+
+          {categories.slice(0, categories.length - 1).map((category) => (
+            <NavLink
+              to={`/category/${category.name}`}
+              className={({ isActive }) =>
+                isActive ? isActiveStyle : isNotActiveStyle
+              }
+              onClick={handleCloseSidebar}
+              key={category.name}
+            >
+              {category.name}
+            </NavLink>
+          ))}
         </div>
       </div>
+
+      {user && (
+        <Link
+          to={`user-profile/${user._id}`}
+          className="flex my-5 mb-3 gap-2 items-center"
+        >
+          <img
+            src={user.image}
+            className="w-10 h-10 rounded-full"
+            alt="user-profile"
+          />
+
+          <p>{user.userName}</p>
+        </Link>
+      )}
     </div>
   );
 };
