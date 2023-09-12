076be1ef8767504e8a9c07ed5674ea0d3d8e501f
jeramief <109633173+jeramief@users.noreply.github.com>
2023-09-11 21:52:16 -0400

    navbar

---
+++ wildlifelens_frontend/src/components/CreatePin.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const CreatePin = () => {
+  return (
+    <div>
+      <p>CreatePin</p>
+    </div>
+  );
+};
+
+export default CreatePin;
---
+++ wildlifelens_frontend/src/components/PinDetail.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const PinDetail = () => {
+  return (
+    <div>
+      <p>PinDetail</p>
+    </div>
+  );
+};
+
+export default PinDetail;
