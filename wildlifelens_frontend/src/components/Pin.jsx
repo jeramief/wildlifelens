4b818b6b2d7055786986df04e7287bc315c408c5
jeramief <109633173+jeramief@users.noreply.github.com>
2023-09-14 19:52:48 -0400

    added save image functionality

--- wildlifelens_frontend/src/components/Pin.jsx
+++ wildlifelens_frontend/src/components/Pin.jsx
@@ -1,16 +1,63 @@
 import React, { useState } from "react";
 import { Link, useNavigate } from "react-router-dom";
 import { v4 as uuidv4 } from "uuid";
-import { urlFor } from "../client";
+import { MdDownloadForOffline } from "react-icons/md";
+import { AiTwotoneDelete } from "react-icons/ai";
+import { BsFillArrowUpRightCircleFill } from "react-icons/bs";
 
-const Pin = ({ pin: { postedBy, image, _id, destination } }) => {
+import { client, urlFor } from "../client";
+import { fetchUser } from "../utils/fetchUser";
+
+const Pin = ({ pin: { postedBy, image, _id, destination, save } }) => {
+  const [postHovered, setPostHovered] = useState(false);
+  const [savingPost, setSavingPost] = useState(false);
+  const user = fetchUser();
+  const navigate = useNavigate();
+
+  const alreadySaved = !!(save?.filter(
+    (item) => item.postedBy._id === user.googleId
+  )).length;
+  // 1, [2, 3, 1] -> [1].length -> 1 -> !1 -> false -> !false -> true
+  // 4, [2, 3, 1] -> [].length -> 0 -> !0 -> true -> !true -> false
+
   return (
-    <div>
-      <img
-        src={urlFor(image).width(250).url()}
-        alt="user-post"
-        className="rounded-lg w-full"
-      />
+    <div className="m-2">
+      <div
+        onMouseEnter={() => setPostHovered(true)}
+        onMouseLeave={() => setPostHovered(false)}
+        onClick={() => navigate(`/pin-detail/${_id}`)}
+        className="relative cursor-zoom-in w-auto hover:shadow-lg rounded-lg overflow-hidden transition-all duration-500 ease-in-out"
+      >
+        <img
+          src={urlFor(image).width(250).url()}
+          alt="user-post"
+          className="rounded-lg w-full"
+        />
+        {postHovered && (
+          <div
+            className="absolute top-0 w-full h-full flex flex-col justify-between p-1 pr-2 pt-2 pb-2 z-50"
+            style={{ height: "100%" }}
+          >
+            <div className="flex items-center justify-between">
+              <div className="flex gap-2">
+                <a
+                  href={`${image?.asset?.url}?dl=`}
+                  download
+                  onClick={(e) => e.stopPropagation()}
+                  className="bg-white w-9 h-9 rounded-full flex items-center justify-center text-dark text-xl opacity-75 hover:opacity-100 hover:shadow-md outline-none"
+                >
+                  <MdDownloadForOffline />
+                </a>
+              </div>
+              {alreadySaved?.length !== 0 ? (
+                <button>Saved</button>
+              ) : (
+                <button>Save</button>
+              )}
+            </div>
+          </div>
+        )}
+      </div>
     </div>
   );
 };
