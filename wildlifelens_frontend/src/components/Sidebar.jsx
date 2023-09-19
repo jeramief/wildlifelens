225e2f7b8ef9a6d18073937a966970ed1d47789e
jeramief <109633173+jeramief@users.noreply.github.com>
2023-09-18 23:15:31 -0400

    added search

--- wildlifelens_frontend/src/components/Sidebar.jsx
+++ wildlifelens_frontend/src/components/Sidebar.jsx
@@ -4,20 +4,12 @@ import { RiHomeFill } from "react-icons/ri";
 // import { IoIosArrowForward } from "react-icons/io";
 
 import logo from "../assets/logo.png";
+import { categories } from "../utils/data";
 
 const isNotActiveStyle =
   "flex items-center px-5 gap-3 text-gray-500 hover:text-black transition-all duration-200 ease-in-out capitalize";
 const isActiveStyle =
   "flex items-center px-5 gap-3 font-extrabold border-r-2 border-black transition-all duration-200 ease-in-out capitalize";
-
-const categories = [
-  { name: "Wild Animals" },
-  { name: "Plants" },
-  { name: "Sky" },
-  { name: "Mountains" },
-  { name: "Pets" },
-  { name: "Other" },
-];
 
 const Sidebar = ({ user, closeToggle }) => {
   const handleCloseSidebar = () => {
@@ -61,6 +53,11 @@ const Sidebar = ({ user, closeToggle }) => {
               onClick={handleCloseSidebar}
               key={category.name}
             >
+              <img
+                src={category.image}
+                alt="category"
+                className="w-8 h-8 rounded-full shadow-sm"
+              />
               {category.name}
             </NavLink>
           ))}
